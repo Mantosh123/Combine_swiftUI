0b052ed0dd742efea9695dd600769d4ebc2d8b19
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-08-02 23:39:02 +0530

    Network Call app

---
+++ Combine2_networkCall/Combine2_networkCall/Combine2_networkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine2_networkCallApp.swift
+//  Combine2_networkCall
+//
+//  Created by Mantosh Kumar on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Combine2_networkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
